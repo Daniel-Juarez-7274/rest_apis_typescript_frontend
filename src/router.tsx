8a73745151af742e7548e09d25c0567ab750829d
Daniel-Juarez-7274 <daniel_alz_juarez.07@hotmail.com>
2025-06-12 19:23:28 -0600

    Frontend terminado

---
+++ src/router.tsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Layout from './layouts/Layout'
+import Products, { loader as productsLoader, action as updateAvailabilityAction } from './views/Products'
+import NewProduct, { action as newProductAction } from './views/NewProduct'
+import EditProduct, { loader as editProductLoader, action as editProductAction } from './views/EditProduct'
+import { action as deleteProductAction } from './components/ProductDetails'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Products />,
+                loader: productsLoader,
+                action: updateAvailabilityAction,
+                hydrateFallbackElement: <p>Cargando...</p>
+            },
+            {
+                path: 'productos/nuevo',
+                action: newProductAction,
+                element: <NewProduct />
+            },
+            {
+                path: 'productos/:id/editar', //ROA Pattern - Resoerced-oriented design
+                element: <EditProduct />,
+                loader: editProductLoader,
+                action: editProductAction
+            },
+            {
+                path: 'productos/:id/eliminar',
+                action: deleteProductAction
+            }
+        ]
+    }
+])
